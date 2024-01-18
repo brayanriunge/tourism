35f2431b1ca921a25cf9a5576fc92c7b10a951b9
RiugeBrian <riungebrayandev@gmail.com>
2024-01-19 00:03:54 +0300

    creating navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,26 @@
+import useMediaQuery from "@/hooks/useMediaQuery";
+import { useState } from "react";
+import { HiMenu } from "react-icons/hi";
+
 export default function Navbar() {
   const flexBetween = "flex items-center justify-between";
+  const isAboveMediumScreen = useMediaQuery("min-h-width: 720px");
+  const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false);
   return (
     <div className={`${flexBetween} fixed z-30 top-0 shadow bg-gray-800 py-6 `}>
       <div className={`${flexBetween} mx-auto w-full`}>
-        <div className={`${flexBetween} w-full gap-10`}></div>
+        <div className={`${flexBetween} w-full gap-10`}>
+          {/**logo */}
+
+          {/**on the left */}
+          {isAboveMediumScreen ? (
+            <div></div>
+          ) : (
+            <button onClick={() => setIsMenuToggled(!isMenuToggled)}>
+              <HiMenu className="h-6 w-6" />
+            </button>
+          )}
+        </div>
       </div>
     </div>
   );

8e63c989a5f708f5d37cfc14e33fa5ace82d667c
RiugeBrian <riungebrayandev@gmail.com>
2024-01-18 23:19:09 +0300

    creating navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,10 @@
+export default function Navbar() {
+  const flexBetween = "flex items-center justify-between";
+  return (
+    <div className={`${flexBetween}`}>
+      <div className={`${flexBetween}`}>
+        <div className={`${flexBetween}`}></div>
+      </div>
+    </div>
+  );
+}

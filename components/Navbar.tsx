871060d017269dc2b55d693fb469e50a9345cb66
RiugeBrian <riungebrayandev@gmail.com>
2024-01-19 21:53:18 +0300

    modifying

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,25 +7,45 @@ import Logo from "@/public/Logo.jpeg";
 
 export default function Navbar() {
   const flexBetween = "flex items-center justify-between";
-  const isAboveMediumScreen = useMediaQuery("min-h-width: 1060px");
+  const isAboveMediumScreen = useMediaQuery("(min-width: 1060px)");
   const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false);
   return (
     <div
-      className={`${flexBetween} fixed z-30 top-0 shadow bg-gray-800 py-6  w-full`}
+      className={`${flexBetween} fixed z-30 top-0 shadow bg-blue-200 py-4  w-full`}
     >
-      <div className={`${flexBetween} mx-auto w-full`}>
+      <div className={`${flexBetween} mx-auto w-5/6`}>
         <div className={`${flexBetween} w-full gap-10`}>
           {/**logo */}
           <Link href="/ ">
-            <Image src={Logo} height={24} width={24} alt="logo" />
+            <Image
+              className="rounded-full"
+              src={Logo}
+              height={64}
+              width={64}
+              alt="logo"
+            />
           </Link>
           {/**on the left */}
           {isAboveMediumScreen ? (
             <div>
-              <Link href={"/"}> home</Link>
+              <Link href={"/"}>
+                <p>Home</p>
+              </Link>
+              <Link href={"/"}>
+                <p>Our team</p>
+              </Link>
+              <Link href={"/"}>
+                <p>Contact Us</p>
+              </Link>
+              <Link href={"/"}>
+                <p>About Us</p>
+              </Link>
             </div>
           ) : (
-            <button onClick={() => setIsMenuToggled(!isMenuToggled)}>
+            <button
+              className="rounded-md"
+              onClick={() => setIsMenuToggled(!isMenuToggled)}
+            >
               <HiMenu className="h-6 w-6" />
             </button>
           )}

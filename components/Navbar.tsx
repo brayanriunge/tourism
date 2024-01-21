13eea1e4560bc396221663c263a23bbd7cea14f5
RiugeBrian <riungebrayandev@gmail.com>
2024-01-21 13:34:47 +0300

    adding mobile view menu

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,7 +2,7 @@ import useMediaQuery from "@/hooks/useMediaQuery";
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
-import { HiMenu } from "react-icons/hi";
+import { HiMenu, HiOutlineX } from "react-icons/hi";
 import Logo from "@/public/Logo.jpeg";
 
 export default function Navbar() {
@@ -14,13 +14,13 @@ export default function Navbar() {
       className={`${flexBetween} fixed z-30 top-0 shadow bg-blue-200 py-4  w-full`}
     >
       <div className={`${flexBetween} mx-auto w-5/6`}>
-        <div className={`${flexBetween} w-full text-gray-700  gap-8`}>
+        <div className={`${flexBetween} w-full text-gray-700 gap-10 `}>
           {/**logo */}
           <Link href="/ ">
             <Image
               className="rounded-full"
               src={Logo}
-              height={64}
+              height={94}
               width={64}
               alt="logo"
             />
@@ -28,8 +28,8 @@ export default function Navbar() {
           <h2 className="text-3xl font-bold ">African Travels</h2>
           {/**on the left */}
           {isAboveMediumScreen ? (
-            <div className="flex  items-center justify-between">
-              <div className={`${flexBetween} gap-4 `}>
+            <div className="flex w-5/6 items-center justify-between">
+              <div className={`${flexBetween} gap-8`}>
                 <Link href={"/"}>
                   <p>Home</p>
                 </Link>
@@ -51,6 +51,19 @@ export default function Navbar() {
             >
               <HiMenu className="h-6 w-6" />
             </button>
+          )}
+          {/**nobile view */}
+          {isAboveMediumScreen && isMenuToggled && (
+            <div className="fixed right-0 bottom-0 h-ful w-[333px] bg-blue-200">
+              <div className="flex p-4 justify-end">
+                <button onClick={() => setIsMenuToggled(!isMenuToggled)}>
+                  <HiOutlineX className="h-6 w-6" />
+                </button>
+              </div>
+              <div className="flex flex-col gap-4 text-2xl items-center">
+                <Link href={"/"}>Home</Link>
+              </div>
+            </div>
           )}
         </div>
       </div>

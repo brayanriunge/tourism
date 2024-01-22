834195daf67074a56878e9b14b527bdf1097dae2
RiugeBrian <riungebrayandev@gmail.com>
2024-01-22 21:55:15 +0300

    SlideShow component

---
+++ components/SlideShow.tsx
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+
+interface SlideShowProps {
+  slides: string[];
+}
+
+export default function SlideShow({ slides }: SlideShowProps) {
+  const [currentSlide, setCurrentSlide] = useState(0);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentSlide((prev) => (prev + 1) % slides.length);
+    }, 2000);
+    return () => clearInterval(interval);
+  }, [slides]);
+  return <div>slides</div>;
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,12 @@
 import Navbar from "@/components/Navbar";
+import SlideShow from "@/components/SlideShow";
 
 export default function Home() {
-  return <Navbar />;
+  const imageSlides = ["antelope.jpeg", "cheetah.jpeg", "owl.jpeg"];
+  return (
+    <>
+      <Navbar />
+      <SlideShow slide={imageSlides} />
+    </>
+  );
 }

35c84722139dd7aa787a75e668cdca2770772d0a
RiugeBrian <riungebrayandev@gmail.com>
2024-01-24 12:37:03 +0300

    replacing images

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import Navbar from "@/components/Navbar";
 import SlideShow from "@/components/SlideShow";
 
 export default function Home() {
-  const imageSlides = ["antelope.jpeg", "cheetah.jpeg", "owl.jpeg"];
+  const imageSlides = ["lions.jpeg", "giraffe.jpeg", "water.jpeg"];
   return (
     <>
       <Navbar />

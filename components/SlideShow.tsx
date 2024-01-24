8571b0bf73b050cde36b576310604a71f4f5873c
RiugeBrian <riungebrayandev@gmail.com>
2024-01-24 12:55:39 +0300

    adding text on images

--- components/SlideShow.tsx
+++ components/SlideShow.tsx
@@ -15,16 +15,22 @@ export default function SlideShow({ slides }: SlideShowProps) {
   return (
     <div className="max-w-full mx-auto w-full h-screen mt-24 relative">
       {slides.map((slide, index) => (
-        <div
-          key={index}
-          className={`slide absolute h-full w-full bg-cover object-contain bg-no-repeat bg-center ${
-            index === currentSlide ? "opacity-100" : "opacity-0"
-          } transition-opacity duration-100`}
-          style={{
-            backgroundImage: `url(${slide})`,
-            backgroundSize: "contain",
-          }}
-        />
+        <>
+          <div
+            key={index}
+            className={`slide absolute h-full w-full bg-cover object-contain bg-no-repeat bg-center ${
+              index === currentSlide ? "opacity-100" : "opacity-0"
+            } transition-opacity duration-100`}
+            style={{
+              backgroundImage: `url(${slide})`,
+              backgroundSize: "contain",
+            }}
+          />
+          <div className="text-container opacity-50">
+            <h1>Africa travels</h1>
+            <p> we are ready to explore the world with you. </p>
+          </div>
+        </>
       ))}
     </div>
   );

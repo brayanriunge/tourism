f762c754954301d5068d77e67913b9ab38514f37
RiugeBrian <riungebrayandev@gmail.com>
2024-01-26 00:07:08 +0300

    styling

--- components/Airport.tsx
+++ components/Airport.tsx
@@ -3,8 +3,26 @@ import airport from "@/public/airport.jpeg";
 
 export default function Airport() {
   return (
-    <section className="bg-gradient-to-r from-teal-500 to-blue-500  ">
-      <div className="mx-auto  md:flex justify-between items-center gap-16 w-5/6 md:h-5/6 mt-32 mb-0">
+    <section className="bg-gradient-to-r from-teal-500 to-yellow-500  ">
+      <div className="m-16 p-10  md:flex justify-between items-center gap-16 w-5/6 md:h-5/6 mt-0 mb-0">
+        <div className="   ">
+          <h1 className="text-3xl pb-4 font-bold ">
+            Airport
+            <span className="text-red-700 italic"> Transport</span>
+          </h1>
+          <div className="text-lg font-bold  justify-content">
+            Experience seamless and stress-free travel with our exceptional
+            airport transportation services that elevate your journey from
+            touchdown to check-in. Arrive in style and comfort with our fleet of
+            luxurious vehicles, ensuring a smooth transition from the airport to
+            your hotel. We guarantee a prompt and reliable service that
+            complements the sophistication of your trip. Start your adventure
+            the moment you step off the plane, and let us take care of the
+            details, allowing you to focus on enjoying every moment of your
+            travel. Trust us to make your airport transfer a seamless and
+            memorable part of your overall journey.
+          </div>
+        </div>
         <Image
           src={airport}
           height={340}
@@ -12,20 +30,6 @@ export default function Airport() {
           alt="airport"
           className="rounded-xl"
         />
-        <div className="text-lg font-bold">
-          Experience seamless and stress-free travel with our exceptional
-          airport transportation services that elevate your journey from
-          touchdown to check-in. Arrive in style and comfort with our fleet of
-          luxurious vehicles, ensuring a smooth transition from the airport to
-          your hotel. Our professional and courteous chauffeurs are dedicated to
-          providing you with a first-class experience, handling all aspects of
-          your transportation needs. We guarantee a prompt and reliable service
-          that complements the sophistication of your trip. Start your adventure
-          the moment you step off the plane, and let us take care of the
-          details, allowing you to focus on enjoying every moment of your
-          travel. Trust us to make your airport transfer a seamless and
-          memorable part of your overall journey.
-        </div>
       </div>
     </section>
   );

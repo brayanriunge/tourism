484d0b22d88cc8930a79d00dbb9df6b626f1e013
RiugeBrian <riungebrayandev@gmail.com>
2024-02-11 19:21:25 +0300

    adding bg-color

--- components/DestinationClass.tsx
+++ components/DestinationClass.tsx
@@ -7,12 +7,12 @@ type props = {
 export default function DestinationClass({ picture, description }: props) {
   const overLayStyles = `p-5 absolute z-30 flex h-[400px] w-[450px] 
     flex-col items-center justify-center whitespace-normal 
-    bg-primary-500 text-center text-white opacity-0 transition 
+    bg-blue-400 text-center text-white opacity-0 transition 
     duration-500 hover:opacity-90`;
   return (
     <li className="mx-5 relative inline-block h-[380px] w-[450px]">
       <div className={overLayStyles}>
-        <h1>{description}</h1>
+        <h1 className="text-2xl font-bold">{description}</h1>
       </div>
 
       <Image src={picture} alt={`${picture}`} height={380} width={450} />

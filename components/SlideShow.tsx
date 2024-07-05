1d375565e6e88126d691f6cc1698a5319adaa4c2
RiugeBrian <riungebrayandev@gmail.com>
2024-07-05 16:09:39 +0300

    coreul

--- components/SlideShow.tsx
+++ components/SlideShow.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 import { SetStateAction, useEffect, useState } from "react";
 import { Carousel } from "react-bootstrap";
 import Giraffe from "@/public/giraffe.jpeg";
+import Antelope from "@/public/antelope.jpeg";
+import Cheetah from "@/public/cheetah.jpeg";
 import {
   CCarousel,
   CCarouselCaption,
@@ -19,6 +21,18 @@ const slides = [
     alt: "first",
     description: "ready to see the wild ",
   },
+  {
+    src: Antelope.src,
+    label: "First Slide",
+    alt: "first",
+    description: "ready to see the wild ",
+  },
+  {
+    src: Cheetah.src,
+    label: "First Slide",
+    alt: "first",
+    description: "ready to see the wild ",
+  },
 ];
 
 export default function SlideShow() {
@@ -28,51 +42,34 @@ export default function SlideShow() {
   };
 
   return (
-    <CCarousel
-      controls
-      indicators
-      dark
-      transition="crossfade"
-      className=" w-full mt-24 "
-    >
-      {slides.map((slide, index) => (
-        <CCarouselItem
-          key={index}
-          className={`carousel-item ${
-            activeIndex === index ? "carousel-item-active" : ""
-          } mx-auto`}
-        >
-          <CImage src={Giraffe.src} className=" mx-auto w-5/6" />
-          <CCarouselCaption className="flex flex-col items-center justify-center py-10  ">
-            <h5 className="m-6 p-6 font-bold font-mono text-xl">
-              {slide.label}
-            </h5>
-            <p className="m-6 p-6 text-lg font-semibold">{slide.description}</p>
-          </CCarouselCaption>
-        </CCarouselItem>
-      ))}
-    </CCarousel>
-    // <Carousel
-    //   activeIndex={index}
-    //   onSelect={handleSelect}
-    //   className="min-h-screen w-full mt-24 "
-    // >
-    //   <Carousel.Item className="bg-antelope bg-cover bg-center min-h-screen w-5/6 mx-auto">
-    //     <Carousel.Caption ">
-    //       <h3 className="m-6 p-6 font-bold font-mono text-xl">site visits</h3>
-    //       <p className="m-6 p-6    text-lg font-semibold">
-    //         ready to visit nice sights with giraffes
-    //       </p>
-    //     </Carousel.Caption>
-    //   </Carousel.Item>
-    //   <Carousel.Item className="bg-antelope bg-cover bg-center min-h-screen w-5/6 mx-auto">
-    //     <Carousel.Caption className="flex flex-col items-center justify-center py-10  ">
-    //       <h3 className="m-6 p-6 font-bold font-mono text-xl">site visits</h3>
-    //       <p className="m-6 p-6    text-lg font-semibold">
-    //         ready to visit nice sights with giraffes
-    //       </p>
-    //     </Carousel.Caption>
-    //   </Carousel.Item>
-    // </Carousel>
+    <div className="relative w-full mt-24">
+      <CCarousel
+        controls
+        indicators
+        dark
+        transition="crossfade"
+        className=" w-full  "
+        activeIndex={activeIndex}
+      >
+        {slides.map((slide, index) => (
+          <CCarouselItem
+            key={index}
+            className={`carousel-item ${
+              activeIndex === index ? "carousel-item-active" : ""
+            } mx-auto`}
+          >
+            <CImage src={Giraffe.src} className=" mx-auto w-5/6" />
+            <CCarouselCaption className="flex flex-col items-center justify-center py-10  ">
+              <h5 className="m-6 p-6 font-bold font-mono text-xl">
+                {slide.label}
+              </h5>
+              <p className="m-6 p-6 text-lg font-semibold">
+                {slide.description}
+              </p>
+            </CCarouselCaption>
+          </CCarouselItem>
+        ))}
+      </CCarousel>
+    </div>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,13 @@ import Destination from "@/components/Destination";
 import Layout from "@/components/Layout";
 import Navbar from "@/components/Navbar";
 import SlideShow from "@/components/SlideShow";
+import SlideShows from "@/components/SlideShows";
 
 export default function Home() {
   const imageSlides = ["lions.jpeg", "giraffe.jpeg", "water.jpeg"];
   return (
     <Layout>
-      <SlideShow slides={imageSlides} />
+      <SlideShow />
       <Airport />
       <Destination />
     </Layout>
